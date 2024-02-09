33ab3f9e2ed250845583b5fa406174b555fba365
Daniel Fenske <dlfenske.fenske@gmail.com>
2024-02-09 10:59:17 -0600

    Initialize TypeScript into the /server directory of the project and update .env variable names to align with postgres; update README/example.env to reflect these changes

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.SERVER_PORT || 9090;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Example app');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
